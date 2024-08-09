dd995bb0c4a3deb448725aa6480809575dbc1dfd
dongxiaofan <394276877@qq.com>
2024-08-09 11:00:00 +0800

    init

---
+++ theme.js
@@ -0,0 +1,41 @@
+export const defaultTheme = {
+  'primary-color': '#008eff', // 全局主色
+  'link-color-default': '#999', // 链接色-默认
+  'link-color-hover': '#008eff', // 链接色-鼠标经过
+  'success-color': '#52c41a', // 成功色
+  'warning-color': '#faad14', // 警告色
+  'error-color': '#ff7875', // 错误色
+  'font-size-base': '14px', // 主字号
+  'heading-color': 'rgba(0, 0, 0, 0.85)', // 标题色
+  'text-color': 'rgba(0, 0, 0, 0.65)', // 主文本色
+  'text-color-secondary': 'rgba(0, 0, 0, 0.45)', // 次文本色
+  'disabled-color': 'rgba(0, 0, 0, 0.25)', // 失效文字色
+  'disabled-background-color': '#f5f5f5', // 失效背景色
+  'disabled-border-color': '#d9d9d9', // 失效背景色
+  'border-radius': '50px', // 组件/浮层圆角
+  'border-color-base': '#d9d9d9', // 边框色
+  'box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)', // 浮层阴影
+  'menu-sider-backgound': '#001529',  // 菜单栏背景色
+  // 'menu-sider-selected-backgound': '#000c17', // 选中的菜单栏item背景色
+  'menu-sider-selected-backgound': '#008eff', // 选中的菜单栏item背景色
+  'menu-sider-selected-text-color': '#fff', // 选中的菜单栏item文字颜色
+  'menu-sider-title-color': '#fff', // 菜单栏title文字颜色
+  'menu-sider-title-active-color': '#fff', // 菜单栏鼠标点击后的文字颜色
+  'pagination-item-default-color': '#666', // 分页默认文字颜色
+  'pagination-item-active-color': '#fff', // 分页当前文字颜色
+  'layout-header-backgound': '#fff', // layout-header背景色
+  'layout-header-border-color': '#eee', // layout-header线条色
+  'layout-header-icon-color': 'rgba(0,0,0,.85)', // layout-header-icon颜色
+  'layout-header-name-color': '#333', // layout-header-name颜色
+  'layout-footer-color': '#999', // layout页脚文字颜色
+  'layout-footer-a-color': '#666', // layout页脚链接颜色
+  'login-footer-color': 'rgba(255, 255, 255, 0.7)', // login页脚颜色
+  'login-input-txt-color': 'rgba(255, 255, 255, 0.7)', // login输入框文字颜色
+  'login-input-icon-color': '#008eff', // login输入框图标颜色
+  'login-input-border-color': 'rgba(0, 131, 175, 0.3)', // login输入框线条颜色
+  'default-gost-button-text-color': 'rgba(0,0,0,.85)', // 灰色幽灵按钮文字
+  'default-gost-button-border-color': '#d9d9d9', // 灰色幽灵按钮线条  
+  'table-selected-background-color': '#e5f8ff', // table选择行的背景色
+  'table-sub-title-border-color': '#74eaf2', // sub-title边框色
+  'table-sub-title-background-color': '#e5f8ff', // sub-title背景色
+}
